235bfcee0a61b1277e9fb153d413fb879654d760
Sayali <sayalipatil263@gmail.com>
2025-10-31 20:30:17 -0500

    added extract_video_data

--- video_stats.py
+++ video_stats.py
@@ -71,6 +71,50 @@ def get_video_ids(playlist_id):
         raise e
 
 
+ 
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id: video_id + batch_size]  #Instead of returning all results at once (like return), it pauses the function and returns one item (here, a batch of IDs) each time.
+            #maximum size is 50 therefore batch will be of 50
+    try:
+        for batch in batch_list(video_ids, Max_Results):  #first executes for first batch then second and so on..
+            video_ids_str = ",".join(batch)  #",".join(batch) converts the list ['id1','id2'] → 'id1,id2'
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            response = requests.get(url)   #Retrieve data from a server (read-only), it stores the data plus the response info in response
+
+            response.raise_for_status() #Do nothing if the request was successful (status code 200–299). Raise an error (requests.exceptions.HTTPError) if the status code indicates a failure (400 or 500 series).
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None)
+                }  
+
+                extracted_data.append(video_data)  
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e  
+
 if __name__ == "__main__": #is to make sure that some code only runs when you run the file directly,and does not run when the file is imported into another file.
     playlist_id = get_playlist_id()
-    get_video_ids(playlist_id)
+    video_ids = get_video_ids(playlist_id)
+    extract_video_data(video_ids)
